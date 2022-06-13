3abbefbbae3f2a54eef47340fe4fc198aa25d3be
agus suhardi <agus.suhardi@tsmid.com>
2022-06-13 20:58:05 +0700

    init

---
+++ src/main/java/com/agussuhardi/restapisimulator/security/ConvertUtil.java
@@ -0,0 +1,50 @@
+package com.agussuhardi.restapisimulator.security;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConvertUtil {
+
+  public static Object mapToObject(Map<String, Object> map) {
+    final ObjectMapper mapper = new ObjectMapper();
+    return mapper.convertValue(map, Object.class);
+  }
+
+  public static Map<String, Object> objectToMap(Object object) {
+    final ObjectMapper mapper = new ObjectMapper();
+    return mapper.convertValue(object, new TypeReference<>() {});
+  }
+
+  public static Map<String, Object> jsonToMap(String json){
+    final ObjectMapper mapper = new ObjectMapper();
+    try {
+      return mapper.readValue(json, new TypeReference<>() {});
+    } catch (JsonProcessingException e) {
+      return new HashMap<>();
+    }
+  }
+
+  public static Object jsonToObject(String json) throws JsonProcessingException {
+    final ObjectMapper mapper = new ObjectMapper();
+    return mapper.readValue(json, Object.class);
+  }
+
+  public static Map<String, Object> base64ToMap(String base64) throws IOException {
+    byte[] decoded = Base64.decodeBase64(base64);
+    String json = new String(decoded, StandardCharsets.UTF_8);
+    return new ObjectMapper().readValue(json, new TypeReference<Map<String, Object>>() {});
+  }
+
+  public static String objectToJson(Object object) throws IOException {
+    ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+    return ow.writeValueAsString(object);
+  }
+}
---
+++ src/main/java/com/agussuhardi/restapisimulator/security/CustomHttpRequestWrapper.java
@@ -0,0 +1,127 @@
+package com.agussuhardi.restapisimulator.security;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+/**
+ * revisi/log_notif 03/09/21
+ *
+ * @author agussuhardi
+ */
+@Slf4j
+public class CustomHttpRequestWrapper extends HttpServletRequestWrapper {
+
+  private final String bodyInStringFormat;
+
+  private final Map<String, Object> body;
+
+  public CustomHttpRequestWrapper(HttpServletRequest request) throws IOException {
+    super(request);
+    bodyInStringFormat =
+        readInputStreamInStringFormat(
+            request.getInputStream(), Charset.forName(request.getCharacterEncoding()));
+
+    log.info(
+        "request from => {hostname: {}, port: {}, httpMethod: {}, url: {}, requestDate:{}, Body: {}}",
+        request.getRemoteHost(),
+        request.getServerPort(),
+        request.getMethod(),
+        request.getRequestURI(),
+        LocalDateTime.now(),
+        bodyInStringFormat);
+
+    body = ConvertUtil.jsonToMap(bodyInStringFormat);
+
+    //    System.out.println("=+++++++++++++++++++++++++++++" + appCode);
+  }
+
+  public Map<String, Object> getBody() {
+    return body;
+  }
+
+  private String readInputStreamInStringFormat(InputStream stream, Charset charset)
+      throws IOException {
+    final int MAX_BODY_SIZE = 1024;
+    final StringBuilder bodyStringBuilder = new StringBuilder();
+    if (!stream.markSupported()) {
+      stream = new BufferedInputStream(stream);
+    }
+
+    stream.mark(MAX_BODY_SIZE + 1);
+    final byte[] entity = new byte[MAX_BODY_SIZE + 1];
+    final int bytesRead = stream.read(entity);
+
+    if (bytesRead != -1) {
+      bodyStringBuilder.append(new String(entity, 0, Math.min(bytesRead, MAX_BODY_SIZE), charset));
+      if (bytesRead > MAX_BODY_SIZE) {
+        bodyStringBuilder.append("...");
+      }
+    }
+    stream.reset();
+
+    return bodyStringBuilder.toString();
+  }
+
+  @Override
+  public BufferedReader getReader() throws IOException {
+    return new BufferedReader(new InputStreamReader(getInputStream()));
+  }
+
+  @Override
+  public ServletInputStream getInputStream() throws IOException {
+    final ByteArrayInputStream byteArrayInputStream =
+        new ByteArrayInputStream(bodyInStringFormat.getBytes());
+
+    return new ServletInputStream() {
+      private boolean finished = false;
+
+      @Override
+      public boolean isFinished() {
+        return finished;
+      }
+
+      @Override
+      public int available() throws IOException {
+        return byteArrayInputStream.available();
+      }
+
+      @Override
+      public void close() throws IOException {
+        super.close();
+        byteArrayInputStream.close();
+      }
+
+      @Override
+      public boolean isReady() {
+        return true;
+      }
+
+      @Override
+      public void setReadListener(ReadListener readListener) {
+        throw new UnsupportedOperationException();
+      }
+
+      public int read() throws IOException {
+        int data = byteArrayInputStream.read();
+        if (data == -1) {
+          finished = true;
+        }
+        return data;
+      }
+    };
+  }
+
+  //  private void send(HttpServletRequest request) {
+  //    response.setStatus(UNAUTHORIZED.value());
+  //    response.setContentType(APPLICATION_JSON_VALUE);
+  //    response.getWriter().write(new GlobalApiResponse<>(UNAUTHORIZED, e.getMessage()).toJson());
+  //  }
+}
